68257af7c3912c214f8486b905da67572273072e
Nona Yerbiz <jonah.crawford@icloud.com>
2022-11-07 16:02:35 +0700

    run the docker build if it doesn't exist

--- run.py
+++ run.py
@@ -3,11 +3,23 @@ import sys
 import sh
 import os
 import time
+import json
+
+def get_dckr_images():
+  return [x.strip() for x in sh.docker.image.ls('--format', '{{json .}}').strip().split('\n')]
+
+def build_gunserver():
+  build = True
+  for line in dckr_images():
+    entry = json.loads(line)
+    if entry.get('Repository') == 'gunrunner/gundb':
+      build = False
+  if not build:
+    sh.docker.build('.',  '-t', 'gunrunner/gundb', _cwd='./servers/docker')
+
 
 def parcel_with_parcelmw():
-
   cwd ='./packagers/parcel'
-    
   container_id = sh.docker.run(
     '-d', '--rm', '--name', 'gunrunner', '-p', '8765:8765', 'gunrunner/gundb', 
     _cwd=cwd, _bg=True, _env=os.environ
@@ -21,11 +33,14 @@ def parcel_with_parcelmw():
   npm_install(_cwd=cwd, _out=sys.stdout, _bg=False, _env=os.environ)
   sh.npm.run('serve', _cwd=cwd, _bg=False, _out=sys.stdout, _env=os.environ)
 
-  # time.sleep(10)
-  # sh.docker.stop(container_id)
+  time.sleep(10)
+  sh.docker.stop(container_id)
 
 def main():
-  parcel_with_parcelmw()
+  build_gunserver()
+  # parcel_with_parcelmw()
+
+
 
 
 
